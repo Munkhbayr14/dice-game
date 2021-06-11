d5325e4d88da5d78b053a4aba6ad8b7c726f3e43
GD <monhbayrm14@gmail.com>
2021-06-11 15:14:53 +0800

    Eelj solih

--- app.js
+++ app.js
@@ -14,16 +14,38 @@ var roundScores = 0;
 // <div class="player-score" id="score-0">43</div>
 
 // 2-р хэсэг Эвэнт DОМ
-ment.getElementById("score-0").textContent = "0";
+document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
-
+diceDom.style.display = "none";
+// шоог шидэх Эвент листэнэр
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 хүртэл санамсргүй тоо гргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+  //Шооны зургийг Эвент дээр гаргаж ирнэ
   diceDom.style.display = "block";
+  //Буусан санамсаргүй тооны зургийг вэб дээр гаргаж ирнэ
   diceDom.src = "dice-" + diceNumber + ".png";
+  //   Буусан тоо нь 1 - ээс ялгаатай бол нэмэгдүүлнэ
+  if (diceNumber !== 1) {
+    roundScores = roundScores + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent =
+      roundScores;
+  } else {
+    roundScores = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    // Улаан цэгийг хөдөлгөх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
 });
